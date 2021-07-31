ed20aa3603ee78b354c687b1f3483e46236e6e08
Jorge E. Espadas <al050610@uacam.mx>
2021-07-31 00:19:54 -0500

    Esqueleto Server Express.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// DEVELOPMENT BRANCH
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        mensaje: "RECEIVED"
+    });
+});
+
+app.listen(80, ()=>{
+    console.log('Server iniciado.');
+});
