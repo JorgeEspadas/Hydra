5b4bb0697ae2712149e778bfd3a1be0757dbdab2
Jorge E. Espadas <al050610@uacam.mx>
2021-08-06 21:49:52 -0500

    /api route

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const apiRoute = require('./routes/api/api');
+
+app.use(express.json());
+app.use('/api', apiRoute);
 
 app.get('/', (req, res) => {
     res.status(200).json({
--- routes/api/api.js
+++ routes/api/api.js
@@ -1,1 +1,10 @@
-// EMPTY - MASTER BRANCH
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) =>{
+    res.status(200).json({
+        message: 'Bienvenido a /api perro :v' // cambiar este mensaje :v
+    });
+});
+
+module.exports = router;
