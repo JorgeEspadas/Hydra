26646113fac5f687c6ed48a7754351df216f919f
Jorge E. Espadas <al050610@uacam.mx>
2021-08-06 20:59:56 -0500

    Cambios

---
+++ models/admin/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+    email : {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    telefono : {
+        type: String,
+        required: true
+    },
+    token: {
+        type: String
+    }
+});
+
+module.exports = mongoose.model('Users', UserSchema);
