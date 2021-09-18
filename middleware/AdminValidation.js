a15e5854d31ea423bdcb8e87e517e6f4085e8213
Jorge E. Espadas <al050610@uacam.mx>
2021-09-18 17:57:46 -0500

    return next -> next

--- middleware/AdminValidation.js
+++ middleware/AdminValidation.js
@@ -18,7 +18,7 @@ module.exports = function(req,res,next) {
         req.user = {
             'email' : 'devmode@observatorio.xyz'
         };
-        return next();
+        next();
     } // hace skip a la validacion de admin si esta encendido.
 
     if(!token) {
