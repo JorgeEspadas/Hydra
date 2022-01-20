4c0dff87109e08f7bb109cd74f0a0c9c4e08a000
Jorge <al050610@uacam.mx>
2022-01-19 20:36:55 -0600

    in-dev: api validate.

--- routes/api/api.js
+++ routes/api/api.js
@@ -5,7 +5,7 @@ const responseHandler = require('../../util/web_responses');
 const config = require('../../util/config');
 const log = require('../../util/log');
 const publicRoute = require('./public/public')
-const {IESPreguntas, IESestudiantes} = require('../../data/DataIES')
+const { IESPreguntas, IESestudiantes } = require('../../data/DataIES')
 
 router.use('/public', publicRoute);
 
@@ -18,43 +18,33 @@ router.post('/validate', async (req, res) => {
         // decrypt, reduce, reencrypt and update (returning the correct token)
         var token = lookup.token;
         var decodedToken = config.decryptJWT(token);
-        // decodedToken.usos--;
-
-        // if(decodedToken.usos < 0){
-        //     await Temp.deleteOne({hash: req.body.key});
-        //     res.status(200).json(responseHandler.errorResponse({message: 'Llave invalida o expirada'}));
-        //     return;
-        // }
-
-        // var reencodedToken = config.generateJWT(decodedToken);
-        // await Temp.updateOne({"hash": req.body.key}, {$set: {token: reencodedToken}}, {upsert: true}, function (err) {
-        //     if(!err){
-        //         res.status(200).json(responseHandler.validResponse({token: reencodedToken}));
-        //     }else{
-        //         log.warning('API', 'Error: '+err);
-        //         res.status(200).json(responseHandler.errorResponse({message: 'Error al actualizar documento'}));
-        //     }
-        // })
 
         // ROL 0 - ALUMNOS, 1  - IES, 2 - EMPRESAS.
-        if (decodedToken.usos > 0) {
-            switch (decodedToken.rol) {
-                case 0:
-                    res.status(200).json(responseHandler.validResponse({
-                        "preguntas" : IESestudiantes
-                    }));
-                    break;
-                case 1:
-                    res.status(200).json(responseHandler.validResponse({
-                        "preguntas" : IESPreguntas
-                    }));
-                    break;
-                case 2:
-                    // algo para empresas
-                    break;
+        try {
+            if (parseInt(decodedToken.usos) > 0) {
+                switch (decodedToken.rol) {
+                    case '0':
+                        res.status(200).json(responseHandler.validResponse({
+                            "preguntas": IESestudiantes
+                        }));
+                        break;
+                    case '1':
+                        console.log('IES!');
+                        res.status(200).json(responseHandler.validResponse({
+                            "preguntas": IESPreguntas
+                        }));
+                        break;
+                    case '2':
+                        // algo para empresas
+                        break;
+                    default: console.log(decodedToken.rol); break;
+                }
+            } else {
+                res.status(200).json(responseHandler.errorResponse({ message: "Este token ha caducado o no tiene mas usos." }))
             }
-        }else{
-            res.status(200).json(responseHandler.errorResponse({message: "Este token ha caducado o no tiene mas usos."}))
+        } catch (e) {
+            res.status(200).json({ message: "fail" });
+            console.log(e);
         }
     }
 });
